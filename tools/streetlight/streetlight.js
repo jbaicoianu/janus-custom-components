d834f7134aa64773aaac381df18794b2bd338c5a
James Baicoianu <james_github@baicoianu.com>
2024-11-03 01:25:22 -0800

    Improved <streetlight>

--- tools/streetlight/streetlight.js
+++ tools/streetlight/streetlight.js
@@ -2,16 +2,21 @@ room.extendElement('object', 'streetlight', {
   state: 'off',
   brightness: 10,
   model_id: false,
-  light_pos: V(0,0,0),
+  light_pos: V(0, 1.2, .25),
   model_pos: V(0,0,0),
+  light_cone_angle: .25,
+  light_rotation: V(90,0,0),
+  light_shadow: false,
 
   create() {
     this.light = this.createObject('light', {
       light_intensity: 0,
-      light_shadow: true,
+      light_shadow: this.light_shadow,
       light_range: 20,
       col: this.col,
-      pos: V(.25, 1.2, 0)
+      pos: this.light_pos,
+      light_cone_angle: this.light_cone_angle,
+      rotation: this.light_rotation,
     });
 
 
@@ -20,7 +25,9 @@ room.extendElement('object', 'streetlight', {
         id: this.model_id,
         pos: V(0, .5, 0),
         col: V(1,1,1),
-        scale: V(.5)
+        collision_id: 'cylinder',
+        collision_scale: V(.1, 4, .1),
+        collision_pos: V(0,2,0)
       });
     }
     this.setState(this.state);
